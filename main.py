87ac57a3556d7014897d11511c7e8b31e2606eb7
camtil <camilla.tillander@outlook.com>
2026-05-31 12:32:45 +0200

    fixat lite ändringar i game_engine gick inte att spela

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from unittest.main import main
+
 from src.board import Board
 from src.ai_player import AIPlayer
 from src.game_engine import GameEngine
@@ -12,3 +14,6 @@ game_engine = GameEngine(board, ai_player)
 game = CLI_Wrapper(game_engine, board)
 
 game.start_game()
+
+if __name__ == "__main__":
+   main()
--- src/game_engine.py
+++ src/game_engine.py
@@ -1,20 +1,14 @@
 class GameEngine:
 
     def __init__(self, board, ai_player):
-
         self.board = board
         self.ai_player = ai_player
 
     def play_turn(self):
-
-        row, col = self.ai_player.make_move(self.board)
-
-        self.board.place_mark(row, col, "O")
-    
-    def make_ai_move(self):
-
         move = self.ai_player.make_move(self.board)
 
-        if move is not None:
-            row, col = move
-            self.board.place_mark(row, col, "O")
+        if move is None:
+            return
+
+        row, col = move
+        self.board.place_mark(row, col, "O")
